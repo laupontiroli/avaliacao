e184f02090c91ee43cbde333eef21f6581c174b7
laupontiroli <laurapm@al.insper.edu.br>
2024-11-04 10:15:00 -0300

    add função de criar

--- src/main/java/com/example/avaliacao/avaliacao/service/AvaliacaoService.java
+++ src/main/java/com/example/avaliacao/avaliacao/service/AvaliacaoService.java
@@ -1,7 +1,27 @@
 package com.example.avaliacao.avaliacao.service;
 
+import com.example.avaliacao.avaliacao.model.Avaliacao;
+import com.example.avaliacao.avaliacao.repository.AvaliacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class AvaliacaoService {
+
+    @Autowired
+    AvaliacaoRepository avaliacaoRepository;
+
+    public Avaliacao criar(Avaliacao avaliacao) {
+        // validar aq se o filme existe
+        // validar aq se o usuário assistiu o filme
+
+        avaliacao.setId(UUID.randomUUID().toString());
+
+        return avaliacaoRepository.save(avaliacao);
+    }
+
+
+
 }
